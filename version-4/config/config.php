5eb936409fa6bb515b9daabad9ff23f57266d5a3
CosmasAlor <cosmas@cosmas.tech>
2026-05-16 17:50:19 +0200

    Initial commit - Custom Mikhmon Hotspot Monitor
    - Complete Mikhmon v4 with version-3 included
    - Hotspot user management
    - Voucher generation system
    - Live reporting dashboard
    - User profile management
    - API integration ready
    
    Source from running container on VPS 2.58.80.82

---
+++ version-4/config/config.php
@@ -0,0 +1,4 @@
+<?php if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<cosmas','mikhmon>|>m6ClpZKl');
+$data['Cosmas'] = array ('1'=>'Cosmas!10.0.0.2','Cosmas@|@admin','Cosmas#|#m6ClpZKl','Cosmas%Cosmas Wifi','Cosmas^cosmas.hotspot','Cosmas&ssp','Cosmas*0924187684','Cosmas(','Cosmas)','Cosmas=30','Cosmas@!@enable','Cosmas#!#');
+$data['ladu'] = array ('1'=>'ladu!10.0.0.3','ladu@|@admin','ladu#|#m6ClpZKl','ladu%ladu Wifi','ladu^ladu.hotspot','ladu&ssp','ladu*','ladu(','ladu)','ladu=30','ladu@!@disable','ladu#!#');
---
+++ webroot/version-3/include/config.php
@@ -0,0 +1,7 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['ADITY'] = array ('1'=>'ADITY!192.168.1.39','ADITY@|@user2','ADITY#|#b2VjrJacmZ2bmQ==','ADITY%Cicukang.net','ADITY^Cicukang.net','ADITY&Rp','ADITY*10','ADITY(1','ADITY)','ADITY=10','ADITY@!@enable');
+$data['BNPROJECT'] = array ('1'=>'BNPROJECT!103.105.107.154:2002','BNPROJECT@|@bdgdh2022','BNPROJECT#|#mpWZnJlkaGNk','BNPROJECT%Diig Hotspot','BNPROJECT^digi-hotspot.id','BNPROJECT&Rp','BNPROJECT*10','BNPROJECT(1','BNPROJECT)','BNPROJECT=5','BNPROJECT@!@enable');
+$data['Cosmas'] = array ('1'=>'Cosmas!10.0.0.2','Cosmas@|@admin','Cosmas#|#m6ClpZKl','Cosmas%Cosmas Wifi','Cosmas^cosmas.hotspot','Cosmas&ssp','Cosmas*10','Cosmas(1','Cosmas)','Cosmas=5','Cosmas@!@enable');
